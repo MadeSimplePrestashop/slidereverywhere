c71633ffc3c65e09c852fc390333e6277eb2a1a2
Zdeno Kuzmany <kuzmany@gmail.com>
2015-01-24 18:59:51 +0700

    Version 1.2

--- sliderseverywhere.php
+++ sliderseverywhere.php
@@ -23,7 +23,7 @@ class sliderseverywhere extends Module {
     public function __construct() {
         $this->name = 'sliderseverywhere';
         $this->tab = 'front_office_features';
-        $this->version = '1.2';
+        $this->version = '1.2.1';
         $this->author = 'kuzmany.biz/prestashop';
         $this->need_instance = 0;
         $this->module_key = '120f5f4af81ccec25515a5eb91a8d263';
